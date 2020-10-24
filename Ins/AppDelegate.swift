af084b93a43a441a5dbe08365f777cae69c57b5f
marshWhale <39956222+kjb0787@users.noreply.github.com>
2020-10-23 22:18:14 -0700

    pushing other files

--- Ins/AppDelegate.swift
+++ Ins/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "zOaCkrIIok300EXcYLXSonR9MvnO7x5l1xqOwWxV"
+                    $0.clientKey = "rEqJ6TjpCnwBfGXzPehjR1Le8Bl2Aokw7ykxSya8"
+                    $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
